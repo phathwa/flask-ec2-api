49a899d42e16bdbf8684cb622060cf16546f4833
The Merchant <phathwa@gmail.com>
2024-11-23 10:07:50 +0200

    changes

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ resource "aws_security_group" "flask_sg" {
 
   # Flask API (Port 5000)
   ingress {
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
